8c78ace2d0a09db2516fa9dd442d439a0f0795b2
fuadsami <fuadsami5@gmail.com>
2017-05-11 15:56:08 +0300

    fix

---
+++ Salametcom/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Salametcom.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+            public ActionResult Donate()
+        {
+            ViewBag.Message = "المتبرعون";
+
+
+            return View();
+        }
+
+
+
+        public ActionResult Voulnteer()
+        {
+            ViewBag.Message = "معلومات عن برامج التطوع المتوفرة";
+
+            return View();
+        }
+         public ActionResult Adminstration()
+        {
+
+            ViewBag.Message = "نبذة عن المسؤولين القائمين في الجمعية";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Salametcom/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Salametcom.Startup))]
+namespace Salametcom
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
